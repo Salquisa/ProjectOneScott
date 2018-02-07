06a209d321bef45081adf91bc7ff92e895522eda
Scott A <salquisa@hawaii.edu>
2018-02-06 23:29:12 -1000

    If Coding Challenge #1

--- ProjectOne.playground/Contents.swift
+++ ProjectOne.playground/Contents.swift
@@ -26,7 +26,7 @@ func fahrenheitToCelsius(fahrenheit : Double) -> Double {
 }
 var fahrenheit = 50
 var  celsius = fahrenheitToCelsius(fahrenheit:)
-(print); "Today is /(celsius) degrees celsius"
+print ("Today is /(celsius) degrees celsius")
 
 //Boolean Variables Coding Challenge #1
 
@@ -34,3 +34,22 @@ var numberOne = 10
 var numberTwo = 5
 var numberThree = numberOne == numberTwo
 
+//If Coding Challenge #1
+var yay = "I am wide awake"
+var yawn = "I am tired because I did not get enough sleep"
+let length = yay.characters.count
+let lengthTwo = yawn.characters.count
+
+if yay == "I am tired because I did not get enough sleep"
+{
+    print("I am ready to learn")
+}
+
+if yay == "I am wide awake"
+{
+    print("I am ready to learn")
+}
+else
+{
+    print("I am not ready to learn")
+}
